890a5b3156caeff072d0bee6c2511b89bc540873
vajjamahendra <mchowdary972@gmail.com>
2024-04-03 16:21:39 +0100

    imported all the necesarry libararies to the python file.

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, request, jsonify
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from werkzeug.security import check_password_hash
+
 app = Flask(__name__)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
