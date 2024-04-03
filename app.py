c0eaa94278a32951a883dbb29d3c254d6559da13
gunasimhareddy72 <gunasimhareddy72@gmail.com>
2024-04-03 16:37:32 +0100

    API for Authentacting the Admin  username and password.

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 
+#API for Authentacting the Admin
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
 
 
 if __name__ == '__main__':
