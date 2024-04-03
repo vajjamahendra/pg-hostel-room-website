3098837fed0f2a9a6c646dbbf3a490b638e3a5b9
vajjamahendra <mchowdary972@gmail.com>
2024-04-03 16:55:50 +0100

    Added a database connection and API to list all the users.

--- app.py
+++ app.py
@@ -5,12 +5,31 @@ from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 
+conn = psycopg2.connect(
+    dbname="",
+    user="",
+    password="",
+    host=""
+)
+
+
+
+cursor = conn.cursor(cursor_factory=RealDictCursor)
+
 #API for Authentacting the Admin
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
     username = data.get('username')
     password = data.get('password')
+
+
+#API to list all the users
+@app.route('/users',methods=['GET'])
+def get_users():
+    cursor.execute("SELECT * FROM students")
+    students = cursor.fetchall()
+    return jsonify({'results':students})
 
 
 
